1a03a77c9d9fddeac32e8a7dfecb333c8924ca98
dani voro <mr.voro@gmail.com>
2023-08-17 11:37:11 +0200

    exercise finished

--- src/main.js
+++ src/main.js
@@ -1,6 +1,37 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var QRCode = require("qrcode");
-QRCode.toDataURL("I am a pony!", function (err, url) {
-    console.log(url);
-});
+var fs = require("fs");
+var channelUrls = [
+    "https://www.youtube.com/@codeorg",
+    "https://www.youtube.com/@CoderbyteDevelopers",
+    "https://www.youtube.com/@Codesmith",
+    "https://www.youtube.com/@craigndave",
+    "https://www.youtube.com/@decomplexify",
+    "https://www.youtube.com/@Fireship",
+    "https://www.youtube.com/@freecodecamp",
+    "https://www.youtube.com/@funfunfunction",
+    "https://www.youtube.com/@KevinPowell",
+    "https://www.youtube.com/@NetNinja",
+    "https://www.youtube.com/@TraversyMedia",
+    "https://www.youtube.com/@WesBos",
+];
+function extractChannelName(channelUrl) {
+    return channelUrl.slice(25);
+}
+function generateQrCode(channelUrl) {
+    var channelName = extractChannelName(channelUrl);
+    var outputPath = "../outputImages/qrcode_".concat(channelName, ".png");
+    fs.open(outputPath, "w", function (err) {
+        if (err)
+            throw err;
+    });
+    QRCode.toFile(outputPath, channelUrl, {
+        errorCorrectionLevel: "H",
+    }, function (err) {
+        if (err)
+            throw err;
+        console.log("QR code created for ".concat(channelName, "!"));
+    });
+}
+channelUrls.forEach(function (channelUrl) { return generateQrCode(channelUrl); });
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,42 @@
 import * as QRCode from "qrcode";
+import * as fs from "fs";
 
-QRCode.toDataURL("I am a pony!", function (err, url) {
-    console.log(url);
-});
+const channelUrls: string[] = [
+    "https://www.youtube.com/@codeorg",
+    "https://www.youtube.com/@CoderbyteDevelopers",
+    "https://www.youtube.com/@Codesmith",
+    "https://www.youtube.com/@craigndave",
+    "https://www.youtube.com/@decomplexify",
+    "https://www.youtube.com/@Fireship",
+    "https://www.youtube.com/@freecodecamp",
+    "https://www.youtube.com/@funfunfunction",
+    "https://www.youtube.com/@KevinPowell",
+    "https://www.youtube.com/@NetNinja",
+    "https://www.youtube.com/@TraversyMedia",
+    "https://www.youtube.com/@WesBos",
+];
+
+function extractChannelName(channelUrl: string): string {
+    return channelUrl.slice(25);
+}
+
+function generateQrCode(channelUrl: string): void {
+    const channelName = extractChannelName(channelUrl);
+    const outputPath = `../outputImages/qrcode_${channelName}.png`;
+    fs.open(outputPath, "w", function (err) {
+        if (err) throw err;
+    });
+    QRCode.toFile(
+        outputPath,
+        channelUrl,
+        {
+            errorCorrectionLevel: "H",
+        },
+        function (err) {
+            if (err) throw err;
+            console.log(`QR code created for ${channelName}!`);
+        }
+    );
+}
+
+channelUrls.forEach((channelUrl) => generateQrCode(channelUrl));
